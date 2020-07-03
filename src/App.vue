d2a09e111d291729f52f5d7f5a9a67447a612dc7
jennlindley <jennifersu2059@gmail.com>
2020-07-03 08:00:51 -0500

    backup

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
 </template>
 
 <style lang="scss">
+@import url("../node_modules/bootstrap/dist/css/bootstrap.css");
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/ListPhotos.vue
+++ src/components/ListPhotos.vue
@@ -3,9 +3,10 @@
     <h1>{{ msg }}</h1>
     <div class="container">
       <div>
+        <button @click="getImages()">Imagenes</button>
 
       </div>
-      <Stack
+      <!-- <Stack
       :column-min-width="300"
       :gutter-width="15"
       :gutter-height="15"
@@ -18,13 +19,13 @@
       >
       <img :src="image.urls.small" :alt="image.alt_description"/>
       </Stack-Item>
-      </Stack>
+      </Stack> -->
     </div>
   </div>
 </template>
 
 <script>
-import { Stack, StackItem } from 'vue-stack-grid';
+// import { Stack, StackItem } from 'vue-stack-grid';
 import getImages from '../unsplash';
 
 export default {
@@ -33,21 +34,30 @@ export default {
     msg: String,
   },
   components: {
-    Stack,
-    StackItem,
+    // Stack,
+    // StackItem,
   },
   methods: {
     getImages,
   },
-  created() {
-    getImages();
-  },
+  // created() {
+  //   getImages();
+  // },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
+h1 {
   margin: 40px 0 0;
+}
+img {
+  width: 300px;
+  height: 225px;
+  margin: 10px;
+}
+.container {
+  width: 80vw;
+  margin: 0 auto;
 }
 </style>
